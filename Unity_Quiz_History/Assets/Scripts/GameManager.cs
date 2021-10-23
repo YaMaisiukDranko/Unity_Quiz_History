5859b2abbe2174111507a3f9af3628d0965a122e
Yaroslav <yarikmeisan@yandex.com>
2021-10-23 09:38:35 +0300

    New System start

--- Unity_Quiz_History/Assets/Scripts/GameManager.cs
+++ Unity_Quiz_History/Assets/Scripts/GameManager.cs
@@ -1,18 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public Question[] questions;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  void Start()
+  {
+    
+  }
 }
